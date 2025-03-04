8569a30e4ad43f87590339344b0c5a8196aa18cf
David <woolsey2316@gmail.com>
2025-03-04 21:28:52 +1100

    wip: correct/incorrect color styling

--- app/components/typing-panel.tsx
+++ app/components/typing-panel.tsx
@@ -4,8 +4,9 @@ import { Cursor } from "./cursor";
 import { WordsToType } from "./words-to-type";
 
 export default function TypingPanel() {
-  const [charIndex, setCharIndex] = useState(0);
   const words = "this is a typing test";
+  const [charIndex, setCharIndex] = useState(0);
+  const [wordsResult, setWordsResult] = useState(Array(words.length));
   return (
     <Box
       sx={{
@@ -18,14 +19,35 @@ export default function TypingPanel() {
         fontFamily: "monospace",
         fontSize: 24,
       }}
+      tabIndex={0}
       onKeyDown={(e) => {
         if (e.key === words[charIndex]) {
           setCharIndex((charIndex) => charIndex + 1);
+          setWordsResult((wordsResult) => {
+            const newWordsResult = [...wordsResult];
+            newWordsResult[charIndex] = "white";
+            console.log(newWordsResult);
+            return newWordsResult;
+          });
+        } else if (e.key === "Backspace") {
+          setCharIndex((charIndex) => charIndex - 1);
+          setWordsResult((wordsResult) => {
+            const newWordsResult = [...wordsResult];
+            newWordsResult[charIndex] = undefined;
+            return newWordsResult;
+          });
+        } else {
+          setCharIndex((charIndex) => charIndex + 1);
+          setWordsResult((wordsResult) => {
+            const newWordsResult = [...wordsResult];
+            newWordsResult[charIndex] = "red";
+            return newWordsResult;
+          });
         }
       }}
     >
-      <Cursor left="-5px" top="-2px" />
-      <WordsToType words={words} />
+      <Cursor left={`${-7 + charIndex * 14}px`} top="-2px" />
+      <WordsToType result={wordsResult} words={words} />
     </Box>
   );
 }
--- app/components/words-to-type.tsx
+++ app/components/words-to-type.tsx
@@ -1,12 +1,24 @@
 interface Props {
   words: string;
+  result: string[];
 }
-export const WordsToType = ({ words }: Props) => {
+export const WordsToType = ({ words, result }: Props) => {
+  let wordsIndex = -1;
   const jsx = words.split(" ").map((word, index) => {
+    wordsIndex++;
     return (
       <div key={index}>
         {word.split("").map((char, charIndex) => {
-          return <span key={index * 10 + charIndex}>{char}</span>;
+          wordsIndex++;
+          console.log(result[wordsIndex]);
+          return (
+            <span
+              style={{ color: result[wordsIndex] }}
+              key={index * 10 + charIndex}
+            >
+              {char}
+            </span>
+          );
         })}
       </div>
     );
